da491f2c615f214edfec1bdd905054bed6223e54
Vova2008zp <ironfoodua@gmail.com>
2022-09-26 18:52:31 +0300

    After checking

--- taskclass.js
+++ taskclass.js
@@ -1,50 +1,55 @@
-'use strict'
+"use strict";
 
 class RangeValidator {
-  constructor (from, to) {
-    if (from >= to || to <= from) {
-      throw new RangeError('Incorrect parameters, expected two numbers');
-    }
-    this._from = from;
-    this._to = to;
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
   }
 
-  get from () {
-  return this._from;
+  get from() {
+    return this._from;
   }
 
-  get to () {
+  get to() {
     return this._to;
   }
-  
-  set from (newFrom) {
-    if(typeof newFrom !== 'number' || isNaN(newFrom)) {
-      throw new TypeError('from isn`t number')
+
+  set from(newFrom) {
+    if (typeof newFrom !== "number" || isNaN(newFrom)) {
+      throw new TypeError("from isn`t number");
+    }
+    if (newFrom > this.to) {
+      throw new RangeError("from must be less than to");
     }
     this._from = newFrom;
   }
 
-  set to (newTo) {
-    if(typeof newTo !== 'number' || isNaN(newTo)) {
-      throw new TypeError('to isn`t number')
+  set to(newTo) {
+    if (typeof newTo !== "number" || isNaN(newTo)) {
+      throw new TypeError("to isn`t number");
+    }
+    if (newTo < this.from) {
+      throw new RangeError("to must be larger than from");
     }
     this._to = newTo;
   }
 
-  get arrayRange () {
-    return [this._from, this._to];
-  };
- 
-
-validate (num) {
-  if (num >= this.from && num <= this.to) {
-    return num;
+  get arrayRange() {
+    return [this.from, this.to];
   }
-  throw new RangeError('Number isn`t in range');
-}
+
+  validate(num) {
+    if (typeof num !== "number" || isNaN(num)) {
+      throw new TypeError("arror valid number");
+    }
+    if (num >= this.from && num <= this.to) {
+      return num;
+    }
+    throw new RangeError("Number isn`t in range");
+  }
 }
 
-const range = new RangeValidator(5,10);
+const range = new RangeValidator(5, 10);
 // const rangeError = new RangeValidator (10,5);
 
 console.log(range.arrayRange);
